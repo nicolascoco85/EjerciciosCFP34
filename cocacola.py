03d4d99665c214a56334af5b2e724649a558635b
Nicolas <nicolascoco85@gmail.com>
2019-04-24 19:54:15 -0300

    juego de acercarse a un numero aleatorio

---
+++ cocacola.py
@@ -0,0 +1,15 @@
+import random
+cantidad_jugadores=int(input('Ingrese el numero de jugadores: '))
+adivinar_numero= random.randrange(0,21)
+nombre= input('ingrese el nombre de jugador: ')
+numero_elegido= int(input('Ingrese un numero: '))
+lista=[]
+turnos_jugados=1
+while (numero_elegido!=adivinar_numero)and (cantidad_jugadores!=turnos_jugados):
+    lista.append((nombre,abs(adivinar_numero-numero_elegido)))
+    nombre = input('ingrese el nombre de jugador: ')
+    numero_elegido = int(input('Ingrese un numero: '))
+    turnos_jugados+=1
+print('El numero misterioso es: ',adivinar_numero)
+for jugador in lista:
+    print(jugador)
