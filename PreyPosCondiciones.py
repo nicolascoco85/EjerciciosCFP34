e5af408ec2c98c6bebdf4ad3d0719ec34a7f4249
nicolas <nicolascoco85>
2019-05-15 17:41:19 -0300

    Se crea una funcion para utilizar como test en el programa principal

--- PreyPosCondiciones.py
+++ PreyPosCondiciones.py
@@ -2,7 +2,10 @@ import JuegosEspeciales as je
 import PRINCIPAL as laura
 import generala_servida_jugada as paulo
 
+def casos_Prueba():
+    print(je.determinarGeneralaServida([4,4,4,4,4],1))
+    print(laura.determinarGeneralaServida([4,4,4,4,4],1))
+    print(paulo.definirGeneralaServida([4,4,4,4,4],1))
 
-print(je.determinarGeneralaServida([4,4,4,4,4],1))
-print(laura.determinarGeneralaServida([4,4,4,4,4],1))
-print(paulo.definirGeneralaServida([4,4,4,4,4],1))
+
+casos_Prueba()
